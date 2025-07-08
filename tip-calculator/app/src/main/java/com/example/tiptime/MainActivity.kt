fe90076449905389491595b46dd7cf8380f1de58
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-07-09 01:37:21 +0530

    tests completed

--- tip-calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ tip-calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -149,7 +150,9 @@ fun TipTimeLayout() {
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(
+
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double, tipPercent: Double = 15.0, roundUp: Boolean
 ): String {
     var tip = tipPercent / 100 * amount
