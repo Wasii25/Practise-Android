b9fc6285e19d35c0fac1d7244a703761fd2f96f2
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-07-10 01:16:34 +0530

    final commit - testing pending

--- ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -7,15 +7,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -61,22 +68,25 @@ fun ArtSpaceApp(
     onNext: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Surface(
-        modifier = modifier
-            .fillMaxSize()
-    ) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly,
             modifier = modifier
+                .widthIn(max = 600.dp)
                 .padding(16.dp)
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
             Image(
                 painter = painterResource(imageRes),
-                contentDescription = null,
+                contentDescription = stringResource(titleRes),
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
+                    .aspectRatio(4f / 3f)
                     .shadow(
                         8.dp,
                         shape = RoundedCornerShape(8.dp)
@@ -84,7 +94,6 @@ fun ArtSpaceApp(
                     .clip(
                         RoundedCornerShape(8.dp)
                     )
-                    .height(250.dp).fillMaxWidth()
                 )
             Column {
                 Text(
@@ -105,22 +114,23 @@ fun ArtSpaceApp(
                 )
             }
             Row(
-
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 Button(
                     onClick = onPrevious,
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f).height(IntrinsicSize.Min),
                 ) {
                     Text(
                         text = stringResource(R.string.previous)
                     )
                 }
 
-                Spacer(modifier = Modifier.width(24.dp))
+
 
                 Button(
                     onClick = onNext,
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f).height(IntrinsicSize.Min),
                 ) {
                     Text(
                         text = stringResource(R.string.next)
@@ -142,7 +152,7 @@ fun GreetingPreview() {
 
 @Composable
 fun ArtSpaceScreen(modifier: Modifier = Modifier) {
-    var currentArtwork by remember { mutableStateOf(1) }
+    var currentArtwork by rememberSaveable { mutableStateOf(1) }
 
     // Map currentArtwork to resources
     val imageRes = when (currentArtwork) {
