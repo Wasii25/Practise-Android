cc2e8174b2d075bcc86489fca1afe5c8b3b7502a
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-07-12 23:06:19 +0530

    final commit - grid view

--- CoursesApp/app/src/main/java/com/example/coursesapp/MainActivity.kt
+++ CoursesApp/app/src/main/java/com/example/coursesapp/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coursesapp.data.DataSource
+import com.example.coursesapp.model.Topic
 import com.example.coursesapp.ui.theme.CoursesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +40,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoursesAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    topicGrid(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +48,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun topicCard(topic: Topic,modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(topic.imageRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(68.dp, 68.dp)
+                    .aspectRatio(1F),
+                contentScale = ContentScale.Crop
+            )
+
+
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(topic.name),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier
+                        .padding(
+                            top = 16.dp,
+                            bottom = 8.dp,
+                            end = 16.dp,
+                        ),
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(
+                                end = 8.dp,
+                            )
+                    )
+                    Text(
+                        text = "${topic.availableCourses} courses",
+                        style = MaterialTheme.typography.labelMedium
+                    )
+
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CoursesAppTheme {
-        Greeting("Android")
+fun topicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2)
+    ) {
+        items(DataSource.topics){ topic ->
+            topicCard(
+                topic,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+
     }
 }
