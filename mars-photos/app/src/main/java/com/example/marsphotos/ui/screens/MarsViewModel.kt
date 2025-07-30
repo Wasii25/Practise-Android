888657083c2da58ec4d196082a375f25a57997cb
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-07-30 20:12:54 +0530

    added json retrifit feature

--- mars-photos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ mars-photos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -22,11 +22,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.marsphotos.network.MarsApi
 import kotlinx.coroutines.launch
+import java.io.IOException
 
+sealed interface MarsUiState {
+    data class Success(val photos: String) : MarsUiState
+    object Loading : MarsUiState
+    object Error : MarsUiState
+}
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
+
 
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
@@ -41,8 +48,14 @@ class MarsViewModel : ViewModel() {
      */
     fun getMarsPhotos() {
         viewModelScope.launch {
-            val listResult = MarsApi.retrofitService.getPhotos()
-            marsUiState = listResult
+            marsUiState = try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                MarsUiState.Success(
+                    "Success: ${listResult.size} Mars photos retrieved"
+                )
+            } catch (e: IOException) {
+                MarsUiState.Error
+            }
         }
     }
 }
