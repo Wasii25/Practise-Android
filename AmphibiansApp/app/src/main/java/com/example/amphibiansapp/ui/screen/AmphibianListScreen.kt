94aab471899fa54a8e00f41c0e5b5f83b9769098
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-08-04 21:33:23 +0530

    commit

--- AmphibiansApp/app/src/main/java/com/example/amphibiansapp/ui/screen/AmphibianListScreen.kt
+++ AmphibiansApp/app/src/main/java/com/example/amphibiansapp/ui/screen/AmphibianListScreen.kt
@@ -1,2 +1,34 @@
 package com.example.amphibiansapp.ui.screen
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PhotosGridScreen(
+    photos: List<Amphbian>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 150.dp),
+        modifier = modifier.padding(horizontal = 4.dp),
+        contentPadding = contentPadding
+    ) {
+        items(items = photos, key = { photo -> photo.id }) { photo ->
+            MarsPhotoCard(
+                photo = photo,
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            )
+        }
+    }
+}
+
+
