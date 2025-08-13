52fc55f71ca9a85fa4bbeb3c15ecedf0021f42fd
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-08-13 22:48:20 +0530

    created database instance

--- Inventory/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ Inventory/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -2,6 +2,7 @@ package com.example.inventory.data
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
@@ -15,7 +16,11 @@ abstract class InventoryDatabase : RoomDatabase() {
         private var Instance: InventoryDatabase? = null
 
         fun getDatabase(context: Context): InventoryDatabase {
-            
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .build()
+                    .also { Instance = it }
+            }
         }
     }
 }
